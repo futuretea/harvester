51187c75e578246790b6a7bddd42d26fdeb84f8c
futuretea <Hang.Yu@suse.com>
2023-03-29 22:39:31 +0800

    Add csi-driver-config setting validation when vmBackup create
    Signed-off-by: futuretea <Hang.Yu@suse.com>

--- pkg/webhook/resources/virtualmachinebackup/validator.go
+++ pkg/webhook/resources/virtualmachinebackup/validator.go
@@ -1,16 +1,20 @@
 package virtualmachinebackup
 
 import (
+	"encoding/json"
 	"fmt"
 
+	ctlcorev1 "github.com/rancher/wrangler/pkg/generated/controllers/core/v1"
 	admissionregv1 "k8s.io/api/admissionregistration/v1"
 	corev1 "k8s.io/api/core/v1"
 	"k8s.io/apimachinery/pkg/runtime"
+	kubevirtv1 "kubevirt.io/api/core/v1"
 
 	"github.com/harvester/harvester/pkg/apis/harvesterhci.io/v1beta1"
 	ctlharvesterv1 "github.com/harvester/harvester/pkg/generated/controllers/harvesterhci.io/v1beta1"
 	ctlkubevirtv1 "github.com/harvester/harvester/pkg/generated/controllers/kubevirt.io/v1"
 	"github.com/harvester/harvester/pkg/settings"
+	"github.com/harvester/harvester/pkg/util"
 	werror "github.com/harvester/harvester/pkg/webhook/error"
 	"github.com/harvester/harvester/pkg/webhook/indexeres"
 	"github.com/harvester/harvester/pkg/webhook/types"
@@ -25,11 +29,13 @@ func NewValidator(
 	vms ctlkubevirtv1.VirtualMachineCache,
 	setting ctlharvesterv1.SettingCache,
 	vmrestores ctlharvesterv1.VirtualMachineRestoreCache,
+	pvcCache ctlcorev1.PersistentVolumeClaimCache,
 ) types.Validator {
 	return &virtualMachineBackupValidator{
 		vms:        vms,
 		setting:    setting,
 		vmrestores: vmrestores,
+		pvcCache:   pvcCache,
 	}
 }
 
@@ -39,6 +45,7 @@ type virtualMachineBackupValidator struct {
 	vms        ctlkubevirtv1.VirtualMachineCache
 	setting    ctlharvesterv1.SettingCache
 	vmrestores ctlharvesterv1.VirtualMachineRestoreCache
+	pvcCache   ctlcorev1.PersistentVolumeClaimCache
 }
 
 func (v *virtualMachineBackupValidator) Resource() types.Resource {
@@ -67,8 +74,11 @@ func (v *virtualMachineBackupValidator) Create(request *types.Request, newObj ru
 	// If VMBackup is from metadata in backup target, we don't check whether the VM is existent,
 	// because the related VM may not exist in a new cluster.
 	if newVMBackup.Status == nil {
-		_, err = v.vms.Get(newVMBackup.Namespace, newVMBackup.Spec.Source.Name)
+		vm, err := v.vms.Get(newVMBackup.Namespace, newVMBackup.Spec.Source.Name)
 		if err != nil {
+			return werror.NewInvalidError(err.Error(), fieldSourceName)
+		}
+		if err = v.checkBackupVolumeSnapshotClass(vm, newVMBackup); err != nil {
 			return werror.NewInvalidError(err.Error(), fieldSourceName)
 		}
 	}
@@ -80,6 +90,36 @@ func (v *virtualMachineBackupValidator) Create(request *types.Request, newObj ru
 		return werror.NewInvalidError(err.Error(), fieldTypeName)
 	}
 
+	return nil
+}
+
+func (v *virtualMachineBackupValidator) checkBackupVolumeSnapshotClass(vm *kubevirtv1.VirtualMachine, newVMBackup *v1beta1.VirtualMachineBackup) error {
+	csiDriverConfig := make(map[string]settings.CSIDriverInfo)
+	if err := json.Unmarshal([]byte(settings.CSIDriverConfig.Get()), &csiDriverConfig); err != nil {
+		return fmt.Errorf("can't get setting csi-driver-config, err: %+v", err)
+	}
+	for _, volume := range vm.Spec.Template.Spec.Volumes {
+		if volume.PersistentVolumeClaim == nil {
+			continue
+		}
+		pvcNamespace := vm.Namespace
+		pvcName := volume.PersistentVolumeClaim.ClaimName
+		pvc, err := v.pvcCache.Get(pvcNamespace, pvcName)
+		if err != nil {
+			return fmt.Errorf("can't  get pvc %s/%s, err: %+v", pvcNamespace, pvcName, err)
+		}
+		provisioner := util.GetProvisionedPVCProvisioner(pvc)
+		c, ok := csiDriverConfig[provisioner]
+		if !ok {
+			return fmt.Errorf("can't get the config of provisioner %s from csi-driver-config setting", provisioner)
+		}
+		if newVMBackup.Spec.Type == v1beta1.Backup && c.BackupVolumeSnapshotClassName == "" {
+			return fmt.Errorf("can't get the backupVolumeSnapshotClassName of provisioner %s from csi-driver-config setting", provisioner)
+		}
+		if newVMBackup.Spec.Type == v1beta1.Snapshot && c.VolumeSnapshotClassName == "" {
+			return fmt.Errorf("can't get the volumeSnapshotClassName of provisioner %s from csi-driver-config setting", provisioner)
+		}
+	}
 	return nil
 }
 
